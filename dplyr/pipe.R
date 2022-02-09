e5d4b650de5ae5b79f97e333d1369b4b57891518
nw <nw@linecorp.com>
2022-02-09 18:27:59 +0900

    pipe sample

---
+++ dplyr/pipe.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+library(nycflights13)
+
+by_dest <- group_by(flights, dest)
+delay <- summarize(by_dest,
+                   count = n(),
+                   dist = mean(distance, na.rm = TRUE),
+                   delay = mean(arr_delay, na.rm = TRUE)
+                  )
+delay <- filter(delay, count > 20, dest != "HNL")
+
+ggplot(data = delay, mapping = aes(x = dist, y = delay)) +
+  geom_point(aes(size=count), alpha = 1/3) +
+  geom_smooth(se = FALSE)
