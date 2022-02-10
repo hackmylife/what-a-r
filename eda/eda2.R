bd834286243099fedf3fcdbf94710da5b98976e9
nw <nw@linecorp.com>
2022-02-10 22:23:03 +0900

    EDA sample 2

---
+++ eda/eda2.R
@@ -0,0 +1,9 @@
+ggplot(diamonds) +
+  geom_histogram(mapping = aes(x = y), binwidth = 0.5) +
+  coord_cartesian(ylim = c(0, 50))
+
+unusual <- diamonds %>%
+  filter(y < 3 | y > 20) %>%
+  select(price, x, y, z) %>%
+  arrange(y)
+unusual
