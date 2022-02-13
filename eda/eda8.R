b44591696e135e6596b8c3668c2142ac2538dcca
nw <nw@linecorp.com>
2022-02-13 23:35:39 +0900

    EDA sample 8

---
+++ eda/eda8.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+library(modelr)
+
+mod <- lm(log(price) ~ log(carat), data = diamonds)
+
+diamonds2 <- diamonds %>%
+  add_residuals(mod) %>%
+  mutate(resid = exp(resid))
+
+ggplot(data = diamonds2) +
+  geom_point(mapping = aes(x = carat, y = resid))
