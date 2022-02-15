e58f94885cba16b49ce9bbb2c5fcdefdf994a3d3
nw <nw@linecorp.com>
2022-02-15 23:05:36 +0900

    tibble sample2

---
+++ tibble/tibble2.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+
+tibble(
+  a = lubridate::now() + runif(1e3) * 86400,
+  b = lubridate::today() + runif(1e3) * 30,
+  c = 1:1e3,
+  d = runif(1e3),
+  e = sample(letters, 1e3, replace=T)
+)
