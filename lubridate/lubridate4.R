9eb940fbba563acb3711ff4191c2277a20bbbac2
nw <nw@linecorp.com>
2022-03-01 23:58:16 +0900

    floor

---
+++ lubridate/lubridate4.R
@@ -0,0 +1,26 @@
+library(tidyverse)
+library(nycflights13)
+library(lubridate)
+
+make_detetime_100 <- function(year, month, day, time) {
+  make_datetime(year, month, day, time %% 100, time %% 100)
+}
+
+frights_dt <- flights %>%
+  filter(!is.na(dep_time), !is.na(arr_time)) %>%
+  mutate(
+    dep_time = make_detetime_100(year, month, day, dep_time),
+    arr_time = make_detetime_100(year, month, day, arr_time),
+    sched_dep_time = make_detetime_100(
+      year, month, day, sched_dep_time
+    ),
+    shed_arr_time = make_detetime_100(
+      year, month, day, sched_arr_time
+    )
+  ) %>%
+  select(origin, dest, ends_with("delay"), ends_with("time"))
+
+frights_dt %>%
+  count(week = floor_date(dep_time, "week")) %>%
+  ggplot(aes(week, n)) +
+    geom_line()
